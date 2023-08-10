3eb0febdfa4a2d44f44188fef475e297170695e4
Nicholas <nsanaie@unc.edu>
2023-08-10 07:27:02 -0400

    v0.2 fix

--- src/Components/buttons/endButton.js
+++ src/Components/buttons/endButton.js
@@ -6,7 +6,7 @@ export default class EndButton extends Component {
 
     render() {
         return (
-            <button href="#" className="testing" data-text={this.props.content} onClick={this.props.onClickHandler}>
+            <button href="#" className="testing2" data-text={this.props.content} onClick={this.props.onClickHandler}>
                 {this.props.content}
             </button>
         );
